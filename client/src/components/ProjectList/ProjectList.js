84690231c4c041925df312e36e33156d3ff05ed5
majorazero <daniel.cw.hsu@gmail.com>
2018-10-29 11:42:53 -0700

    adding project funtionality written

--- client/src/components/ProjectList/ProjectList.js
+++ client/src/components/ProjectList/ProjectList.js
@@ -1,16 +1,33 @@
 import React, {Component} from "react";
 import Modal from '@material-ui/core/Modal';
+import axios from "axios";
 require("./ProjectList.css");
 
 class ProjectList extends Component{
   state = {
     projectName: "",
     summary: "",
-    dueDate: ""
+    dueDate: "",
+    projects: ""
   }
+
+
 
   handleChange = (event) => {
     this.setState({[event.target.name]:event.target.value});
+  }
+  handleSubmit= (event) => {
+    event.preventDefault();
+    //we'll create a project now.
+    let dateFormat = this.state.dueDate;
+    console.log(dateFormat);
+    axios.post("/api/project",{
+      name: this.state.projectName,
+      summary: this.state.summary,
+      dueDate: this.state.dueDate
+    }).then((response) => {
+      console.log(response.data);
+    });
   }
 
   render(){
@@ -18,9 +35,9 @@ class ProjectList extends Component{
       <div className="projList">
         <h1>This is a project List.</h1>
         <div>
-
+          Imagine a scroll bar here.
         </div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <h2>Someone(Mike) might have to material-ui this into a modal/drawer for me.</h2>
           <h2>Project Name</h2>
           <input type="text" name="projectName" onChange={this.handleChange}/>
@@ -28,6 +45,9 @@ class ProjectList extends Component{
           <textarea name="summary" onChange={this.handleChange}/>
           <h2>Due Date</h2>
           <input type="date" name="dueDate" onChange={this.handleChange}/>
+          <div>
+            <button type="Submit">Submit</button>
+          </div>
         </form>
       </div>
     );
