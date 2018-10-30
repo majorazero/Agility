98716adefa2b3b3a951dd7869da3d53a3bc62b68
YJ <yair.yoseph@gmail.com>
2018-10-30 13:23:23 -0700

    404 page done

---
+++ client/src/components/utils/404/404.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import ContainedButtons from "../../utils/Button.js";
+
+
+
+function notFound() {
+    return (
+        <div style={{
+            backgroundImage: `url("/assets/images/404.png")`,
+            maxHeight: "100%",
+            height: "100vh",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: "rgb(6, 5, 5,9.5)",
+            zIndex: 5,
+            display: "flex",
+            overflow: "hidden"
+
+        }}>
+
+            <div style={{
+                margin: "500px",
+                zIndex: 10,
+            }}>
+                <ContainedButtons
+                    component={Link}
+                    to="/"
+                    name="Take Me Home"
+                    color="secondary"
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center"
+                    }}
+                />
+
+            </div>
+        </div >
+    )
+}
+
+
+export default notFound;
