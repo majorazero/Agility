61dfcf0116de0d513997311c5b4b82fc8bd15b83
rezamike <michael.sanaiha@gmail.com>
2018-10-30 16:10:10 -0700

    add project button

--- client/src/components/ProjectList/ProjectList.js
+++ client/src/components/ProjectList/ProjectList.js
@@ -1,15 +1,23 @@
-import React, {Component} from "react";
-import {Link} from "react-router-dom";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Grid } from "@material-ui/core";
 import axios from "axios";
 import ProjectListTab from "./ProjectListTab/ProjectListTab.js";
+import SimpleModalProjectWrapped from "../utils/ModalProject.js";
+import AddProjectLayout from "../utils/AddProjectLayout.js";
+import ButtonSizes from "../utils/FAB.js";
 require("./ProjectList.css");
 
-class ProjectList extends Component{
+class ProjectList extends Component {
   state = {
-    projectName: "",
+    name: "",
     summary: "",
-    dueDate: "",
-    projects: []
+    due_date: "",
+    projects: [],
+    open: false,
+    direction: 'column',
+    justify: 'flex-start',
+    alignItems: 'flex-start',
   }
 
   componentDidMount = () => {
@@ -17,34 +25,34 @@ class ProjectList extends Component{
   }
 
   fetch = () => {
-    axios.post("/api/projectOfUser",{
+    axios.post("/api/projectOfUser", {
       id: sessionStorage.getItem("id"),
       token: localStorage.getItem("token")
     }).then((response) => {
-      this.setState({projects: response.data});
+      this.setState({ projects: response.data });
     });
   }
 
   populate = () => {
-    if(this.state.projects.length === 0){
+    if (this.state.projects.length === 0) {
       //maybe ill replace this with something if no projects appeared yet.
       return <h1>Oops no projects yet.</h1>;
     }
-    else{
+    else {
       return this.state.projects.map((item) => {
         return <ProjectListTab
           key={item.id}
           name={item.name}
           summary={item.summary}
-          duedate={item.due_date}
-          onProjectPress = {() => {this.onProjectPress(item.id)}} />;
+          due_date={item.due_date}
+          onProjectPress={() => { this.onProjectPress(item.id) }} />;
       });
     }
   }
 
   //we'll pass project id into this and link it to a specific project page
   onProjectPress = (id) => {
-    axios.post("/api/encrypt",{
+    axios.post("/api/encrypt", {
       token: localStorage.getItem("token"),
       id: id.toString()
     }).then((data) => {
@@ -52,16 +60,21 @@ class ProjectList extends Component{
     });
   }
 
-  handleChange = (event) => {
-    this.setState({[event.target.name]:event.target.value});
-  }
-  handleSubmit= (event) => {
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    }, () => {
+      console.log(this.state.due_date)
+    });
+  };
+
+  handleSubmit = (event) => {
     event.preventDefault();
     //we'll create a project now.
-    axios.post("/api/project",{
-      name: this.state.projectName,
+    axios.post("/api/project", {
+      name: this.state.name,
       summary: this.state.summary,
-      due_date: this.state.dueDate,
+      due_date: this.state.due_date,
       id: sessionStorage.getItem("id"),
       token: localStorage.getItem("token")
     }).then((response) => {
@@ -69,25 +82,57 @@ class ProjectList extends Component{
     });
   }
 
-  render(){
-    return(
-      <div className="projList">
-        <h1>This is a project List.</h1>
+  handleOpen = () => {
+    this.setState({
+      open: true
+    })
+  }
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { direction, justify, alignItems } = this.state;
+    return (
+      <div>
+        <Grid
+          container
+          alignItems={alignItems}
+          direction={direction}
+          justify={justify}
+        >
+          <ButtonSizes
+            onClick={this.handleOpen}
+          />
+          <SimpleModalProjectWrapped
+            open={this.state.open}
+            onClose={this.handleClose}
+            name="Add a New Project ..."
+            onSubmit={this.handleSubmit}
+            onChange={this.handleChange}
+          >
+
+            <AddProjectLayout
+            />
+          </SimpleModalProjectWrapped>
+          {/* <h1>This is a project List.</h1>
         <div>
           {this.populate()}
         </div>
         <form onSubmit={this.handleSubmit}>
           <h2>Someone(Mike) might have to material-ui this into a modal/drawer for me.</h2>
           <h2>Project Name</h2>
-          <input type="text" name="projectName" onChange={this.handleChange}/>
-          <h2>Summary Name</h2>
+          <input type="text" name="name" onChange={this.handleChange}/>
+          <h2>summary Name</h2>
           <textarea name="summary" onChange={this.handleChange}/>
           <h2>Due Date</h2>
-          <input type="date" name="dueDate" onChange={this.handleChange}/>
+          <input type="date" name="due_date" onChange={this.handleChange}/>
           <div>
             <button type="Submit">Submit</button>
           </div>
-        </form>
+        </form> */}
+        </Grid>
       </div>
     );
   }
