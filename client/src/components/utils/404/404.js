5a4bbffc4bc7637cd40a20805cf562d1b22052d2
majorazero <daniel.cw.hsu@gmail.com>
2018-10-31 22:49:59 -0700

    404 pages now takes you out after 5.5 seconds

--- client/src/components/utils/404/404.js
+++ client/src/components/utils/404/404.js
@@ -1,41 +1,57 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from "react-router-dom";
 import ContainedButtons from "../../utils/Button.js";
 
 
 
-function notFound() {
-  return (
-      <div style={{
-          backgroundImage: `url("/assets/images/404.png")`,
-          maxHeight: "100%",
-          height: "100vh",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          backgroundColor: "rgb(6, 5, 5,9.5)",
-          zIndex: 5,
-          display: "flex",
-          overflow: "hidden"
-      }}>
-      <div style={{
-          margin: "500px",
-          zIndex: 10,
-      }}>
-        <ContainedButtons
-          component={Link}
-          to="/"
-          name="Take Me Home"
-          color="secondary"
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-          }}
-        />
+class notFound extends Component{
 
+  state = {
+    message: "Take me home!"
+  }
+
+  componentDidMount = () => {
+    setTimeout(()=>{
+      this.setState({message:"Time to go!"});
+    },5000);
+    setTimeout(()=>{
+      this.props.history.push("/");
+    },5500);
+  }
+
+  render(){
+    return (
+        <div style={{
+            backgroundImage: `url("/assets/images/404.png")`,
+            maxHeight: "100%",
+            height: "100vh",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize:"cover",
+            backgroundColor: "rgb(6, 5, 5,9.5)",
+            zIndex: 5,
+            display: "flex",
+            overflow: "hidden"
+        }}>
+        <div style={{
+            margin: "500px",
+            zIndex: 10,
+        }}>
+          <ContainedButtons
+            component={Link}
+            to="/"
+            name={this.state.message}
+            color="secondary"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          />
+        </div>
       </div>
-    </div>
-  )
+    );
+  }
 }
 
 
