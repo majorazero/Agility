a46ea450c224cf988702eb08ce5dbeefe9b84535
majorazero <daniel.cw.hsu@gmail.com>
2018-11-27 16:18:07 -0800

    initial made

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -34,7 +34,8 @@ class Homepage extends Component {
       projectLoaded: false,
       userFirstName: "",
       userLastName: "",
-      userEmail: ""
+      userEmail: "",
+      initial: ""
     }
   }
 
@@ -66,6 +67,7 @@ class Homepage extends Component {
             userFirstName: response.data.prof.first_name,
             userLastName: response.data.prof.last_name,
             userEmail: response.data.prof.email,
+            initial: `${response.data.prof.first_name.charAt[0]} ${response.data.prof.last_name.charAt[0]}`,
             summaryLoaded: true
           }
         );
